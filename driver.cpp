4d378e6292d8322d5d84ccd81d30653fb5463d1e
Benjamin Clark <127693639+benrc257@users.noreply.github.com>
2024-10-09 19:55:57 +0000

    adding file searching

--- driver.cpp
+++ driver.cpp
@@ -10,8 +10,17 @@
 #include "Classes/sorter.h"
 
 int main() {
+    // Variables
+    vector<string> filenames;
+    string extension = ".csv";
+    fs::directory_entry directory(StorageInput);
 
-    
+    // Adding filenames to the vector for menu
+    if (directory.exists()) { //if StorageInput directory exists
+
+    } else { //if StorageInput directory does not exist, creates it and returns
+        fs::create_directory(StorageInput);
+    }
 
     return 0;
 }
--- main.h
+++ main.h
@@ -5,8 +5,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+using namespace std;
+
+
+// Includes
 #include <iostream>
 #include <fstream>
 #include <filesystem>
+#include <vector>
+
+
+// Global Constants
+const string StorageInput = "Storage/Playlists";
+const string StorageOutput = "Storage/SortedPlaylists";
+namespace fs = filesystem;
 
 #endif
